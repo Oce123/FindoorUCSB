963a126797b87f8b56427fb143a8a4e58a485eb4
oceane <oceane.champagne@edu.esiee.fr>
2023-12-11 18:28:31 -0800

    Find a professor fait plus sa salle plus office hours

--- app/src/main/java/com/example/findoor/ProfessorActivity.java
+++ app/src/main/java/com/example/findoor/ProfessorActivity.java
@@ -86,11 +86,81 @@ public class ProfessorActivity extends AppCompatActivity {
 
                     ArrayAdapter<String> adapter = new ArrayAdapter<>(ProfessorActivity.this, android.R.layout.simple_list_item_1, professorDetailsList);
                     listView.setAdapter(adapter);
+
+                    // Add click listener to ListView items
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            // Get the selected item (professor name)
+                            String selectedItem = (String) parent.getItemAtPosition(position);
+                            String professorName = selectedItem.split(" - ")[0];
+
+                            // Fetch and display office hours for the selected professor
+                            fetchAndDisplayOfficeHours(professorName);
+                        }
+                    });
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    private void fetchAndDisplayOfficeHours(String professorName) {
+        // Perform a new request to get office hours for the selected professor
+        String[] field = new String[]{"name"};
+        String[] data = new String[]{professorName};
+        PutData putData = new PutData("http://192.168.5.69/FindoorDatabase/getofficehours.php", "POST", field, data);
+
+        if (putData.startPut()) {
+            if (putData.onComplete()) {
+                String result = putData.getResult();
+
+                // Parse JSON array for office hours
+                try {
+                    JSONArray jsonArray = new JSONArray(result);
+
+                    // Create a list to display office hours
+                    List<String> officeHoursList = new ArrayList<>();
+
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                        String officeHours = jsonArray.getString(i);
+                        officeHoursList.add(officeHours);
+                    }
+
+                    // Display office hours in a dialog
+                    showOfficeHoursDialog(officeHoursList);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void showOfficeHoursDialog(List<String> officeHoursList) {
+        // Create a dialog to display office hours
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Office Hours");
+
+        // Convert office hours list to a single string for display
+        StringBuilder officeHoursStringBuilder = new StringBuilder();
+        for (String officeHours : officeHoursList) {
+            officeHoursStringBuilder.append(officeHours).append("\n");
+        }
+
+        builder.setMessage(officeHoursStringBuilder.toString());
+
+        // Add a button to close the dialog
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // Close the dialog
+                dialog.dismiss();
+            }
+        });
+
+        // Display the dialog
+        builder.create().show();
     }
 
 
